c8146e85238d61affda4841009fca27bb6bf1634
alexlarosa <larosa.alexandra@gmail.com>
2021-03-17 17:49:49 -0400

    Added search engine

--- src/app.js
+++ src/app.js
@@ -15,21 +15,17 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+
 function displayTemperature(response){
 
 let dateElement = document.querySelector("#date");
 dateElement.innerHTML = formatDate(response.data.dt* 1000);
-
 document.querySelector("#city").innerHTML = response.data.name;
-
 let tempElement = document.querySelector("#temperature");
 tempElement.innerHTML = Math.round(response.data.main.temp);
-
 document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].description;
-
 document.querySelector("#windSpeed").innerHTML = Math.round(response.data.wind.speed);
-
 let iconElement = document.querySelector("#weatherIcon")
 ;iconElement.setAttribute(
     "src", 
@@ -38,8 +34,22 @@ let iconElement = document.querySelector("#weatherIcon")
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+
+function search(city) {
 let apiKey = "aea833a90485bad517aeb7963cee7156";
-let city = "Madrid";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#search-input");
+    console.log(cityInputElement.value);
+search(cityInputElement.value);
+}
+
+search("Madrid");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
